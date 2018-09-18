1f5dfdfc12386a4dad61849df3f2c84d472d7193
Nathan Higley <lune@quebec.ksd.org>
2018-09-18 08:53:48 -0700

    simpler revision

---
+++ js/load.js
@@ -0,0 +1,9 @@
+function load(page) {
+  var id = page.dataset.id;
+  var list = document.querySelectorAll(".show");
+
+  list.forEach(function(item) {
+    item.classList.remove("show");
+  });
+  document.getElementById(id).classList.add("show");
+}
--- js/title.js
+++ js/title.js
@@ -3,12 +3,10 @@ var titles = new Array(
   'When in Rome, Sink Ships',
   'TUMS&#174 Festival',
   'Another Happy Landing',
+  'D U M P&nbsp&nbspI T',
+  'I&nbsp&nbspS A I D',
+  'Gives Me Conniptions',
+  'Don\'t Wake Up Tomorrow',
+  'Better Than Ovaltine',
   'Nice Talkin\' To Ya',);
 var title = titles[parseInt(Math.random() * titles.length)];
-
-//SUBTITLE
-var subtitles = new Array(
-    'Icon of the Hubris',
-    'Hambone Extraordinaire',
-    'The Guy That Isn\'t The Other Guy');
-var subtitle = subtitles[parseInt(Math.random() * subtitles.length)];
