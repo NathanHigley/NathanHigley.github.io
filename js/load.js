243777af294411892a54a38812c25b26f34be2ae
NathanHigley <nathan@jhigley.com>
2018-10-10 10:04:04 -0700

    Mr. Banks, your opening statement.

--- js/load.js
+++ js/load.js
@@ -1,49 +1,8 @@
-function push(page) {
-
-  var pid = page.dataset.id
-  var elem = document.getElementById(pid)
-  var list = document.querySelectorAll(".show")
-  var pos = -200;
-  var ext = 280;
-
-  function move() {
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (pos != ext) {
-        pos = pos + 20;
-      }
-      else {
-        clearInterval(id);
-      }
-      elem.style.top = pos + 'px';
-    }
-  }
-
-  function load() {
-    list.forEach(function(item) {
-      item.classList.remove("show");
-    });
-    document.getElementById(pid).classList.add("show");
-  }
-  load()
-  move()
-}
-
-function pull(page) {
-
-  var pid = page.dataset.id
-  var elem = document.getElementById(pid)
-  var list = document.querySelectorAll(".show")
-
-  function back() {
-    elem.style.top = '-200px';
-  }
-
-  function unload() {
-    list.forEach(function(item) {
-      item.classList.remove("show");
-    })
-  }
-  back()
-  unload()
+function swap(page) {
+  var id = page.dataset.id;
+  var list = document.querySelectorAll(".show");
+  list.forEach(function(item) {
+    item.classList.remove("show");
+  });
+  document.getElementById(id).classList.add("show");
 }
