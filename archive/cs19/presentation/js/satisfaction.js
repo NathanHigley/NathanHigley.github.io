61f1bc0bf3f82a20b936b30edee6b8acfdebb646
NathanHigley <nathan@jhigley.com>
2019-05-14 10:15:44 -0700

    presentated

--- archive/cs19/presentation/js/satisfaction.js
+++
@@ -1,7 +0,0 @@
-function satisfaction(){document.write(`
-  <img style="box-shadow:0px 0px 5px grey;border:solid grey 3px;border-radius:10px;height:auto;max-width:800px;"src="img/Ob.jpg"/><br /><br />
-  <div class="link_con">
-  <h1>Satisfaction and Plans</h1><hr />
-  <h4>Problem-solving and debugging has been very satisfying. It grants</h4>
-  </div>
-`);}
